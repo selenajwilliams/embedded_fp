a0a3b75a766b3216d0ae29bba828054ba5b2504a
selenajoetwilliams <selenajoetwilliams@gmail.com>
2023-12-07 16:46:23 -0500

    [feat] modified beat selection so that beats are not selected more than 1 per second

--- src/wav_to_bpm.py
+++ src/wav_to_bpm.py
@@ -1,23 +1,54 @@
 import librosa
 import librosa.beat as beat
+import numpy as np
+
+
+def extract_audio_data(file_path):
+    bpm = extract_bpm(file_path)
+    beats, num_beats = calculate_peaks(file_path)
+
+    return bpm, beats, num_beats
+
 
 def extract_bpm(file_path):
     audio, sample_rate = librosa.load(file_path)
     onset_env = librosa.onset.onset_strength(y=audio, sr=sample_rate)
-    tempo, beats = beat.beat_track(onset_envelope=onset_env, sr=sample_rate)
+    # instead of using beats from beat_track, we use peak_pick in calculate_peaks
+    # which gives more control over how many beats per second are selected,
+    # selecting only the biggest/loudest/main beats    
+    tempo, beats = beat.beat_track(onset_envelope=onset_env, sr=sample_rate,
+                                   units='time', start_bpm=86)
+    """ get the bpm """
     bpm = round(tempo) # round float to nearest int
 
+    return bpm
 
-    beats_string = str(beats).strip("[]")
 
-    num_beats = len(beats)
-    # beats_string = ' '.join(str(beats)) # pre-processing beats
-    # beats_string.strip("[]")
+def calculate_peaks(input_file):
+    y, sr = librosa.load(input_file)
+    onset_env = librosa.onset.onset_strength(y=y, sr=sr,
+                                         hop_length=512,
+                                         aggregate=np.median)
+    # custom params so that peaks don't occur > 1 every second
+    peaks = librosa.util.peak_pick(onset_env, pre_max=3, post_max=3, 
+                                   pre_avg=3, post_avg=5, delta=0.5, wait=100)
+    peak_times = librosa.frames_to_time(peaks, sr=sr)
 
-    print(beats_string)
+    # convert to millis & round to nearest integer
+    rounded_peaks = []
+    for x in peak_times:
+        r_peak = x * 1000
+        r_peak = round(r_peak)
+        rounded_peaks.append(r_peak)
 
-    return bpm, beats_string, num_beats
+    print("detecting beats...")
+    print(f"{len(peak_times)} peaks found")
+    print(peak_times)
+
+    num_peaks = len(peak_times)
+
+    return rounded_peaks, num_peaks
 
 if __name__ == "__main__":
     wav_file = 'panama.wav'
-    extract_bpm(wav_file)
+    extract_audio_data(wav_file)
