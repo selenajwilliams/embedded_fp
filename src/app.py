26cac3da61eac65960dc8bff788d9f6bc9a7d5f0
selenajoetwilliams <selenajoetwilliams@gmail.com>
2023-12-07 14:33:34 -0500

    [doc] added requirements.txxt

--- src/app.py
+++ src/app.py
@@ -16,27 +16,29 @@ app = Flask(__name__)
 
 @app.before_request
 def before_request():
-    bpm, beats = wav_to_bpm.extract_bpm("panama.wav")
+    bpm, beats, num_beats = wav_to_bpm.extract_bpm("panama.wav")
+
     g.bpm = bpm
     g.beats = beats
+    g.num_beats = num_beats
 
 @app.route("/")
 def homepage():
     return f"<p>homepage\nBPM: {g.bpm}\nBeats: {g.beats}</p>"
 
-@app.route("/bpm")
+@app.route("/bpm") # note: I am not using this endpoint currently
 def bpm():
-    return f"<p>BPM: {g.bpm}</p>"
+    return f"<p>{g.bpm}</p>"
 
 @app.route("/beats")
 def beats():
-    return f"<p>Beats: {g.beats}</p>"
+    return f"<p>{g.beats}</p>"
+
+@app.route("/num_beats")
+def num_beats():
+    print(g.num_beats)
+    return f"<p>{g.num_beats}</p>"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
-
-
-
-
-
-
--- src/wav_to_bpm.py
+++ src/wav_to_bpm.py
@@ -7,8 +7,17 @@ def extract_bpm(file_path):
     tempo, beats = beat.beat_track(onset_envelope=onset_env, sr=sample_rate)
     bpm = round(tempo) # round float to nearest int
 
-    return bpm, beats
+
+    beats_string = str(beats).strip("[]")
+
+    num_beats = len(beats)
+    # beats_string = ' '.join(str(beats)) # pre-processing beats
+    # beats_string.strip("[]")
+
+    print(beats_string)
+
+    return bpm, beats_string, num_beats
 
 if __name__ == "__main__":
-    wav_file = 'metronome100bpm.wav'
+    wav_file = 'panama.wav'
     extract_bpm(wav_file)
